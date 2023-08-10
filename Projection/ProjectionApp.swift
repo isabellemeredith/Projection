0589c5bfe9a09d623ebf9c3f22239842533c1951
Isabelle Meredith <isme6117@colorado.edu>
2023-08-10 15:06:04 -0600

    Initial Commit

---
+++ Projection/ProjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectionApp.swift
+//  Projection
+//
+//  Created by Isabelle on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
